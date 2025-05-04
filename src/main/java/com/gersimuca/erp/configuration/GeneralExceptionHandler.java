a13315d0b183b0930a6e6654544a2d6d88a96982
〽️ Gersi Muça <gm96key@gmail.com>
2025-05-04 12:25:26 +0200

    Feature/test (#68)
    * tests
    
    * format
    
    * fixed tests

--- src/main/java/com/gersimuca/erp/configuration/GeneralExceptionHandler.java
+++ src/main/java/com/gersimuca/erp/configuration/GeneralExceptionHandler.java
@@ -6,13 +6,17 @@ import com.gersimuca.erp.common.util.LoggerUtils;
 import com.gersimuca.erp.model.ApiError;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.NonUniqueObjectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @ControllerAdvice
@@ -42,6 +46,16 @@ public class GeneralExceptionHandler {
       final ConstraintViolationException exception, final WebRequest request) {
     LoggerUtils.exception(log, exception, ErrorSeverity.WARN, request.getDescription(false));
     final HttpStatus badRequestHttpStatus = HttpStatus.BAD_REQUEST;
+
+    final ApiError apiErrorResponse = buildApiErrorResponse(exception, badRequestHttpStatus);
+    return ResponseEntity.status(badRequestHttpStatus).body(apiErrorResponse);
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<ApiError> handleMethodArgumentTypeMismatchException(
+      final MethodArgumentTypeMismatchException exception, final WebRequest request) {
+    LoggerUtils.exception(log, exception, ErrorSeverity.WARN, request.getDescription(false));
+    final HttpStatus badRequestHttpStatus = HttpStatus.BAD_REQUEST;
     final ApiError apiErrorResponse = buildApiErrorResponse(exception, badRequestHttpStatus);
     return ResponseEntity.status(badRequestHttpStatus).body(apiErrorResponse);
   }
@@ -55,6 +69,16 @@ public class GeneralExceptionHandler {
     return ResponseEntity.status(badRequestHttpStatus).body(apiErrorResponse);
   }
 
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiError> handleMethodArgumentNotValidException(
+      final MethodArgumentNotValidException exception, final WebRequest request) {
+    LoggerUtils.exception(log, exception, ErrorSeverity.WARN, request.getDescription(false));
+    final HttpStatus internalServerErrorHttpStatus = HttpStatus.BAD_REQUEST;
+    final ApiError apiErrorResponse =
+        buildApiErrorResponse(exception, internalServerErrorHttpStatus);
+    return ResponseEntity.status(internalServerErrorHttpStatus).body(apiErrorResponse);
+  }
+
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ApiError> handleException(
       final Exception exception, final WebRequest request) {
@@ -63,6 +87,34 @@ public class GeneralExceptionHandler {
     final ApiError apiErrorResponse =
         buildApiErrorResponse(exception, internalServerErrorHttpStatus);
     return ResponseEntity.status(internalServerErrorHttpStatus).body(apiErrorResponse);
+  }
+
+  @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
+  public ResponseEntity<ApiError> handleObjectOptimisticLockingFailureException(
+      final ObjectOptimisticLockingFailureException exception, final WebRequest request) {
+    LoggerUtils.exception(log, exception, ErrorSeverity.WARN, request.getDescription(false));
+    final HttpStatus conflictHttpStatus = HttpStatus.CONFLICT;
+    final ApiError apiErrorResponse = buildApiErrorResponse(exception, conflictHttpStatus);
+    return ResponseEntity.status(conflictHttpStatus).body(apiErrorResponse);
+  }
+
+  @ExceptionHandler(NonUniqueObjectException.class)
+  public ResponseEntity<ApiError> handleNonUniqueObjectException(
+      final NonUniqueObjectException exception, final WebRequest request) {
+    LoggerUtils.exception(log, exception, ErrorSeverity.WARN, request.getDescription(false));
+    final HttpStatus conflictHttpStatus = HttpStatus.CONFLICT;
+    final ApiError apiErrorResponse = buildApiErrorResponse(exception, conflictHttpStatus);
+    return ResponseEntity.status(conflictHttpStatus).body(apiErrorResponse);
+  }
+
+  @ExceptionHandler(org.hibernate.exception.ConstraintViolationException.class)
+  public ResponseEntity<ApiError> handleConstraintViolationException(
+      final org.hibernate.exception.ConstraintViolationException exception,
+      final WebRequest request) {
+    LoggerUtils.exception(log, exception, ErrorSeverity.WARN, request.getDescription(false));
+    final HttpStatus conflictHttpStatus = HttpStatus.CONFLICT;
+    final ApiError apiErrorResponse = buildApiErrorResponse(exception, conflictHttpStatus);
+    return ResponseEntity.status(conflictHttpStatus).body(apiErrorResponse);
   }
 
   private ApiError buildApiErrorResponse(
