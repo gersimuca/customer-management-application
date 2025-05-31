b368fd3f27e557c24b3d72876b68002a01a11a36
GERSI MUCA <gersi.muca@lhind.dlh.de>
2025-05-31 10:38:08 +0200

    added test for the interceptor

--- src/main/java/com/gersimuca/erp/configuration/interceptor/MdcInterceptor.java
+++ src/main/java/com/gersimuca/erp/configuration/interceptor/MdcInterceptor.java
@@ -1,4 +1,4 @@
-package com.gersimuca.erp.configuration;
+package com.gersimuca.erp.configuration.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
