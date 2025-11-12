41df0bc936275a1b96cccda24674f5917f12cb15
〽️ Gersi Muça <gm96key@gmail.com>
2025-11-13 00:48:14 +0100

    Feature/mockito (#204)
    * added sample for mockito
    
    * set the async thread pool as default based on configurations
    
    * added test for async
    
    * more stable rejection policy
    
    * added ADR for centralized asynch
    
    * correcting ADR
    
    * added aop logging the service interface impl this include the execution time
    
    * fixed logging aspect
    
    * added graphql and async configurations
    
    * added graphql and async configurations
    
    * added graphql and async configurations
    
    * formate the code
    
    * running test
    
    * running test
    
    * upgraded to version 3.5.0
    
    * added test for the interceptor
    
    * fixed tests
    
    * fixed tests
    
    * added interceptor on his own package
    
    * added interceptor on his own package
    
    * added metrics test
    
    * added metrics test
    
    * added metrics test for each type
    
    * test signed commit
    
    * test signed commit
    
    * added submodule for user-interface
    
    * rename submodule and project update
    
    * upgraded docker-compose
    
    * Fixed submodule URLs
    
    * added kafka on docker with zookeper
    
    * upgraded to java 25
    
    * upgrade checker
    
    * update readme
    
    * update ADR
    
    * chore: added preconfig grafana graphs
    
    * chore: fixed kafka config issue and upgraded the pipeline
    
    * chore: fixed kafka config issue and upgraded the pipeline
    
    * chore: fixed kafka config issue and upgraded the pipeline
    
    * chore: fixed kafka config issue and upgraded the pipeline
    
    * chore: fixed kafka config issue and upgraded the pipeline
    
    * chore: fixed kafka config issue and upgraded the pipeline
    
    * feat(): setup future documentation

---
+++ docs/proposed/sso-authentication/openid-auth.md
@@ -0,0 +1,3 @@
+## The principle functionality is as follows
+
+https://learn.microsoft.com/en-us/entra/identity-platform/v2-protocols-oidc
--- src/main/java/com/gersimuca/erp/feature/user/UserRepository.java
+++ src/main/java/com/gersimuca/erp/feature/user/UserRepository.java
@@ -1,7 +1,10 @@
 package com.gersimuca.erp.feature.user;
 
 import com.gersimuca.erp.common.repository.BaseRepository;
+import java.time.OffsetDateTime;
 import java.util.Optional;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Window;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +15,6 @@ public interface UserRepository extends BaseRepository<UserEntity, Long> {
   boolean existsByUsername(String username);
 
   Optional<UserEntity> findByUsername(String username);
+
+  Window<UserEntity> getAllByCreatedAt(OffsetDateTime createdAt, ScrollPosition scrollPosition);
 }
