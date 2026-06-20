c44e6f9a300414114f1a17f3040789daa7ccfb16
UB Gersi Muca <gersimuca@aol.com>
2026-06-20 10:23:47 -0400

    feat(): fixed formating issue

--- src/main/java/com/gersimuca/erp/feature/machine_registry/stale/MachineRegistryStaleCleanupServiceImpl.java
+++ src/main/java/com/gersimuca/erp/feature/machine_registry/stale/MachineRegistryStaleCleanupServiceImpl.java
@@ -2,17 +2,16 @@ package com.gersimuca.erp.feature.machine_registry.stale;
 
 import com.gersimuca.erp.common.util.LoggerUtils;
 import com.gersimuca.erp.feature.machine_registry.MachineRegistryService;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class MachineRegistryStaleCleanupServiceImpl implements MachineRegistryStaleCleanupService{
+public class MachineRegistryStaleCleanupServiceImpl implements MachineRegistryStaleCleanupService {
 
   private final MachineRegistryService service;
 
